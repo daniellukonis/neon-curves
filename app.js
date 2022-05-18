5c3d396f8c8407cdd418bd9a9357a794ae27ccad
daniellukonis <daniel.lukonis@gmail.com>
2022-05-18 10:00:45 -0400

    Initial commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-window.addEventListener("contextmenu",e => e.preventDefault())
+// window.addEventListener("contextmenu",e => e.preventDefault())
 
 
 /*-------------------------*/
@@ -8,8 +8,11 @@ const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
 function resizeCanvas(canvas){
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  let length;
+  window.innerWidth > window.innerHeight ? length = window.innerHeight : length = window.innerWidth;
+  length = Math.floor(length * 0.95);
+  canvas.width = length;
+  canvas.height = length;
 }
 resizeCanvas(canvas);
 
@@ -46,7 +49,24 @@ class Point {
     return array;
   }
 
-  static drawBenz(context, pointArray){
+  static createPalette(){
+    const ad = Math.floor(fxrand() * 10) + 1;
+    let a = Math.floor(360 * fxrand());
+    const s = 100
+    const l = 50
+    const cs1 = `hsl(${a}, ${s}%, ${l}%, .30)`
+    const cs2 = `hsl(${a}, ${s}%, ${l}%, .60)`
+    const cs5 = `hsl(${a}, ${s}%, ${l}%)`
+    a += 30 * ad;
+    const cs3 = `hsl(${a}, ${s}%, ${l}%, .60)`
+    const cs4 = `hsl(${a}, ${s}%, ${l}%, .40)`
+    const cs6 = `hsl(${a}, ${s}%, ${l}%)`
+
+    return {cs1,cs2,cs3,cs4,cs5,cs6}
+  }
+
+  static drawBenz(context, pointArray, colorPalette){
+    const cp = colorPalette;
     const c = context;
     const p0 = pointArray[0];
     p0.reverse = true;
@@ -57,10 +77,10 @@ class Point {
 
     c.save();
     const g = c.createLinearGradient(p0.x, p0.y, p1.x, p1.y);
-    g.addColorStop(0, '#FF005530');
-    g.addColorStop(0.2, '#FF005560');
-    g.addColorStop(0.8, '#0000FF60');
-    g.addColorStop(1, '#0000FF40');
+    g.addColorStop(0, cp.cs1);
+    g.addColorStop(0.2, cp.cs2);
+    g.addColorStop(0.8, cp.cs3);
+    g.addColorStop(1, cp.cs4);
     c.strokeStyle = g;
     c.lineWidth = 30;
     c.lineCap = 'round';
@@ -75,8 +95,8 @@ class Point {
 
     c.save();
     const h = c.createLinearGradient(p0.x, p0.y, p1.x, p1.y);
-    h.addColorStop(0, '#FF0055');
-    h.addColorStop(1, '#0000FF')
+    h.addColorStop(0, cp.cs5);
+    h.addColorStop(1, cp.cs6)
     c.strokeStyle = h;
     c.lineWidth = 15;
     c.lineCap = 'round';
@@ -102,10 +122,10 @@ class Point {
     this.canvas = canvas;
     this.context = context;
     this.fullArc = Math.PI * 2;
-    this.x = Math.floor((canvas.width / 2)  * Math.random()) + canvas.width / 4;
-    this.y = Math.floor((canvas.height / 2) * Math.random()) + canvas.height / 4;
+    this.x = Math.floor((canvas.width / 2)  * fxrand()) + canvas.width / 4;
+    this.y = Math.floor((canvas.height / 2) * fxrand()) + canvas.height / 4;
 
-    this.r = Math.floor(canvas.height / 2 * Math.random()) + 20;
+    this.r = Math.floor(canvas.height / 2 * fxrand()) + 20;
     this.rx = 0; //orbit point
     this.ry = 0;
     this.rr = 10;
@@ -117,10 +137,11 @@ class Point {
     this.dy = 0;
 
     this.a = 0;
-    this.ad = Math.random() >= 0.5 ? 1 : -1;
-    this.av = Math.PI / 360 * this.ad * Math.random();
+    this.ad = fxrand() >= 0.5 ? 1 : -1;
+    this.av = Math.PI / 360 * this.ad * fxrand();
 
     this.strokeStyle = '#FFFFFF70';
+    this.lineWidth = 6;
 
     this.reverse = false;
 
@@ -184,9 +205,9 @@ class Point {
 
   drawOrbit({context:c} = this, dash){
     c.save();
-    c.strokeStyle = '#FFFFFF20';
+    c.strokeStyle = '#FFFFFF30';
     c.fillStyle = '#000';
-    c.lineWidth = 6;
+    c.lineWidth = this.lineWidth;
     c.setLineDash(dash || [2,4]);
     c.beginPath();
     c.arc(this.x, this.y, this.r, 0, this.fullArc);
@@ -194,9 +215,24 @@ class Point {
     c.restore();
   }
 
+  drawHighlightOrbit({context:c} = this, dash){
+    const ac = Math.PI / 8
+    c.save();
+    c.strokeStyle = '#FFFFFF20';
+    c.fillStyle = '#000';
+    c.lineCap = 'round';
+    c.lineWidth = this.lineWidth * 2;
+    // c.setLineDash(dash || [2,4]);
+    c.beginPath();
+    c.arc(this.x, this.y, this.r, this.a - ac, this.a + ac );
+    c.stroke();
+    c.restore();
+  }
+
   animate(){
     this.update();
     this.drawOrbit();
+    this.reverse ? null : this.drawHighlightOrbit();
     this.calcCtrlPoint();
     this.calcOppositePoint();
     this.drawCenterLine();
@@ -208,14 +244,80 @@ class Point {
 
 /*-------------------------*/
 
+class OutOrbit{
+  constructor(canvas, context){
+    this.canvas = canvas;
+    this.context = context;
+
+    this.x = this.canvas.width / 2;
+    this.y = this.canvas.height / 2;
+    this.r = this.canvas.width / 2 - 20;
+
+    this.a = 0;
+    this.d = fxrand() >= 0.5 ? 1 : -1;
+    this.av = fxrand() * 0.005 * this.d;
+    this.al = Math.floor(180 * fxrand());
+
+    this.strokeStyle = '#222'
+  }
+  draw({context:c} = this){
+    c.save();
+    c.lineWidth = 20;
+    c.strokeStyle = this.strokeStyle;
+    c.setLineDash([5,5]);
+    c.beginPath();
+    c.arc(this.x, this.y, this.r, this.a, this.a - this.al);
+    c.stroke()
+    c.restore();
+  }
+  update(){
+    this.a += this.av;
+  }
+  animate(){
+    this.update();
+    this.draw();
+
+  }
+}
+
+
+/*-------------------------*/
+
+
 const points = Point.createArray(canvas, context, 4);
+const colorPalette = Point.createPalette();
+
+const orbit = new OutOrbit(canvas, context);
+
+const fps = 60;
+const interval = 1000 / fps;
+let now = Date.now();
+let then = Date.now();
+let delta = 0;
 
 function loop(){
-  requestAnimationFrame(loop);
-  clearCanvas(canvas, context);
-  // fadeCanvas(canvas, context);
-  Point.animate(points);
-  Point.drawBenz(context, points);
-  return
-}
+  animation = requestAnimationFrame(loop);
+  now = Date.now();
+  delta = now - then;
+  if(delta > interval){
+      then = now - (delta % interval);
+      //animation code
+      clearCanvas(canvas, context);
+      orbit.animate();
+      Point.animate(points);
+      Point.drawBenz(context, points, colorPalette);
+    }
+  }
+
 loop();
+
+function radToDeg(rad){
+  const deg = 180 / Math.PI * rad;
+  return deg;
+}
+
+window.$fxhashFeatures = {
+  "Neon color 1": colorPalette.cs5,
+  "Neon color 2": colorPalette.cs6
+// here define the token features
+}
